7a6bd8bae128e935041d4d2d178b56196235259e
mehran <mehran.naseri2013@gmail.com>
2021-01-09 12:15:33 +0330

    eat: add .env file , install some package, add new route to subscribe new topic

--- server.js
+++ server.js
@@ -1,35 +1,46 @@
-const SOCKET_PORT = 8080;
+require('dotenv').config();
+const SOCKET_PORT = process.env.SOCKET_PORT;
 const REDIS = {
-    "host": "127.0.0.1",
-    "port": "6379",
-    "password": "",
+    "host": process.env.REDIS_HOST,
+    "port": process.env.REDIS_PORT,
+    "password": process.env.REDIS_PASSWORD,
     "family": 4
 };
-function handler(request, res) {
 
-    res.statusCode = 200;
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-}
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-var ioRedis = require('ioredis');
-var redis = new ioRedis(REDIS);
-app.listen(SOCKET_PORT, function() {
-    console.log(new Date + ' - Server is running on port ' + SOCKET_PORT + ' and listening Redis on port ' + REDIS.port + '!');
+var http = require('http');
+
+http.createServer(function (req, res) {
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    var url = req.url;
+    req.on('data', (chunk) => {
+        let body = JSON.parse(chunk);
+        if(url ==='/add') {
+            var app = require('http').createServer();
+            var io = require('socket.io')(app);
+            var ioRedis = require('ioredis');
+            var redis = new ioRedis(REDIS);
+            app.listen(SOCKET_PORT, function() {
+                console.log(new Date + ' - Server is running on port ' + SOCKET_PORT + ' and listening Redis on port ' + REDIS.port + '!');
+            });
+
+            io.on('connection', function(socket) {
+                console.log('A client connected');
+            });
+
+            redis.psubscribe(body['topic'], function(err, count) {
+                console.log('Subscribed a');
+            });
+            redis.on('pmessage', function(subscribed, channel, data) {
+                console.log(data);
+                io.emit(channel , data);
+            });
+            res.write(' Welcome to about us page');
+            res.end();
+        }
+    })
+}).listen(process.env.PORT, function() {
+    console.log("server start at port "+process.env.PORT);
 });
 
-io.on('connection', function(socket) {
-    console.log('A client connected');
-});
-
-redis.psubscribe('*', function(err, count) {
-    console.log('Subscribed a');
-});
-redis.on('pmessage', function(subscribed, channel, data) {
-    // data = JSON.parse(data);
-    console.log(data);
-    io.emit(channel , data);
-});
