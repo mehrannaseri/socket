87bff4bf4be5eac7528b770826f1ef503fe7526d
mehran <mehran.naseri2013@gmail.com>
2021-01-12 17:33:27 +0330

    feat: add dir and file to test

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+const chai = require('chai')
+const expect = chai.expect
+
+
+describe("validator isNumValid()", () => {
+
+    it("should return true", ()=> {
+        expect(true).to.be.true
+    })
+})
